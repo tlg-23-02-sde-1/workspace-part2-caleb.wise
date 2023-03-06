b170af0d4bee0ad7b1146168c5cf247c852ed78f
Caleb Wise <caleb.wise@tlgcohort.com>
2023-03-06 10:53:00 -0800

    Iteration 2 - Board

---
+++ DuckRace/src/com/duckrace/BoardTest.java
@@ -0,0 +1,20 @@
+package com.duckrace;
+
+import static com.duckrace.Reward.*;
+
+class BoardTest {
+
+    public static void main(String[] args) {
+        Board board = new Board();
+
+        //board.dumpStudentIdMap();
+        System.out.println();
+
+        board.update(14, PRIZES);
+        board.update(2, DEBIT_CARD);
+        board.update(1, PRIZES);
+        board.update(6, PRIZES);
+        board.update(1, PRIZES);
+        board.show();
+    }
+}
